1730ccce1df79904234f424ea1e07d9c232239d5
zhuweiyou <i@zhuweiyou.com>
2021-04-16 13:26:02 +0800

    test

--- plugin/flash/service.js
+++ plugin/flash/service.js
@@ -1,9 +1,7 @@
 async function getFlash(http, message) {
   try {
     const file = message.match(/type=flash,file=(.*?)\.image/).pop()
-    console.log('[flash] file', file)
     const { data } = await http.send('get_image', { file: `${file}.image` })
-    console.log('[flash] data', data)
     return [
       {
         type: 'image',
