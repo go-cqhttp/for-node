df5b8c00b39a0715f6cc4719665abe6c3da56b41
zhuweiyou <i@zhuweiyou.com>
2021-04-16 11:59:05 +0800

    fix

--- plugin/recall/index.js
+++ plugin/recall/index.js
@@ -3,10 +3,13 @@ const service = require("./service");
 module.exports = (options) => {
   return async ({ data, ws, http }) => {
     if (data.notice_type === "group_recall") {
-      ws.send("send_group_forward_msg", {
-        group_id: data.group_id,
-        message: [data.message_id],
-      });
+      const message = await service.getRecall(http, data.message_id);
+      if (message) {
+        ws.send("send_group_msg", {
+          group_id: data.group_id,
+          message,
+        });
+      }
       return;
     }
 
