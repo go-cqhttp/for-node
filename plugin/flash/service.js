dfc25ba99a42872bfe1bb69ea95b20c48d29142b
zhuweiyou <i@zhuweiyou.com>
2021-04-16 12:22:18 +0800

    url

--- plugin/flash/service.js
+++ plugin/flash/service.js
@@ -2,12 +2,14 @@ async function getFlash(http, message) {
   try {
     const file = message.match(/type=flash,file=(.*?).image/).pop();
     const { data } = await http.send("get_image", { file: `${file}.image` });
-    return {
-      type: "image",
-      data: {
-        url: data.url,
+    return [
+      {
+        type: "image",
+        data: {
+          url: data.url,
+        },
       },
-    };
+    ];
   } catch (e) {
     console.error("[flash]", e);
     return null;
