9e3b765cda7ffad1c3118737eefc2d23c9386082
zhuweiyou <i@zhuweiyou.com>
2021-04-25 15:21:34 +0800

    优化格式

--- plugin/weibo/README.md
+++ plugin/weibo/README.md
@@ -12,6 +12,8 @@ module.exports = {
 
 用户发送 "微博/热搜/微博热搜/WB/RS/WBRS", 机器人抓取微博热搜 TOP10 返回
 
-![image](https://user-images.githubusercontent.com/8413791/115852493-b8d65c00-a45a-11eb-9127-3c3e5f55ec27.png)
+![image](https://user-images.githubusercontent.com/8413791/115984455-ba7a5e00-a5d9-11eb-98e0-acaaf96ee74e.png)
 
 用户发送 "微博/热搜/微博热搜/WB/RS/WBRS 搜索词", 机器人返回微博搜索地址, 方便群友点击直达
+
+![image](https://user-images.githubusercontent.com/8413791/115984463-c2d29900-a5d9-11eb-8d28-7f917d2f5cc7.png)
--- plugin/weibo/service.js
+++ plugin/weibo/service.js
@@ -8,65 +8,64 @@ const ICON_MAP = {
   hot: '热',
 }
 
+function searchWeibo(q) {
+  return [
+    {
+      type: 'text',
+      data: {
+        text: `https://s.weibo.com/weibo?q=${encodeURIComponent(q)}`,
+      },
+    },
+  ]
+}
+
+async function listResou() {
+  const { data } = await axios(
+    'https://s.weibo.com/top/summary?cate=realtimehot'
+  )
+  const $ = cheerio.load(data)
+  return [...$('.list_a > li')]
+    .slice(0, 20)
+    .map((item, index) => {
+      const title = $(item).find('span').text()
+      const number = $(item).find('span > em').text()
+      return {
+        title: title.replace(number, '').trim(),
+        number,
+        type:
+          index === 0
+            ? '顶'
+            : ICON_MAP[
+                ($(item).find('i').attr('class') || '').replace(
+                  'icon icon_',
+                  ''
+                )
+              ] || '',
+        url: $(item).find('a').attr('href'),
+      }
+    })
+    .filter(item => item.type !== '荐')
+    .slice(0, 10)
+}
+
 async function getList(search) {
   try {
     if (search) {
-      return [
-        {
-          type: 'text',
-          data: {
-            text: `https://s.weibo.com/weibo?q=${encodeURIComponent(search)}`,
-          },
-        },
-      ]
+      return searchWeibo(search)
     }
-
-    const { data } = await axios(
-      'https://s.weibo.com/top/summary?cate=realtimehot'
-    )
-    const $ = cheerio.load(data)
-    const top10 = [...$('.list_a > li')]
-      .slice(0, 20)
-      .map((item, index) => {
-        const title = $(item).find('span').text()
-        const number = $(item).find('span > em').text()
-        return {
-          title: title.replace(number, '').trim(),
-          number,
-          type:
-            index === 0
-              ? '顶'
-              : ICON_MAP[
-                  ($(item).find('i').attr('class') || '').replace(
-                    'icon icon_',
-                    ''
-                  )
-                ] || '',
-          url: $(item).find('a').attr('href'),
-        }
-      })
-      .filter(item => item.type !== '荐')
-      .slice(0, 10)
 
     return [
       {
         type: 'text',
         data: {
           text:
-            '[微博热搜]\n' +
-            top10
-              .map((item, index) =>
-                [index, item.type + item.number, item.title]
-                  .map(item => String(item).trim())
-                  .filter(item => item)
-                  .join(' ')
-              )
-              .join('\n'),
+            '微博热搜\n\n' +
+            (await listResou()).map(item => item.title.trim()).join('\n'),
         },
       },
     ]
   } catch (e) {
-    console.error('[realtimehot]', e)
+    console.error('[weibo]', e)
     return [
       {
         type: 'text',
