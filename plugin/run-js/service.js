c6a71e3d451271031de05f693aa0fb226f0d8c73
zhuweiyou <i@zhuweiyou.com>
2021-04-27 18:25:01 +0800

    优化运行结果过长的刷屏问题

--- plugin/run-js/service.js
+++ plugin/run-js/service.js
@@ -6,11 +6,20 @@ function handleOutput(output) {
     return '运行结果为空'
   }
 
-  if (o.length < 1000) {
-    return o
+  let f = ''
+  o = o.split('\n')
+  if (o.length > 10) {
+    o = o.slice(0, 10)
+    f = '\n(为了避免刷屏, 仅显示前10次换行)'
   }
 
-  return o.slice(0, 1000) + '\n(为了避免刷屏, 仅显示前 1000 字符)'
+  o = o.join('\n')
+  if (o.length > 520) {
+    o = o.slice(0, 520)
+    f = '\n(为了避免刷屏, 仅显示前520个字符)'
+  }
+
+  return o + f
 }
 
 function runJs(input, options) {
@@ -20,9 +29,9 @@ function runJs(input, options) {
         {
           type: 'text',
           data: {
-            text: handleOutput(output)
-          }
-        }
+            text: handleOutput(output),
+          },
+        },
       ])
     }
     try {
@@ -33,7 +42,7 @@ function runJs(input, options) {
         .replace(/&amp;/g, '&')
       const vm = new VM({
         timeout: options.timeout || 1000 * 10,
-        sandbox: options.sandbox || {}
+        sandbox: options.sandbox || {},
       })
       const hasCallback = code.includes('callback(')
       const result = vm.run(
@@ -52,5 +61,5 @@ function runJs(input, options) {
 }
 
 module.exports = {
-  runJs
+  runJs,
 }
