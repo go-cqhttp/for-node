b7bd26d06911d733f8581c89aa16b0c40dba110c
zhuweiyou <i@zhuweiyou.com>
2021-05-22 10:58:02 +0800

    plugin/chives

---
+++ plugin/chives/README.md
@@ -0,0 +1,13 @@
+# chives
+
+> 韭菜插件
+
+```js
+module.exports = {
+  plugin: {
+    './plugin/chives': {},
+  },
+}
+```
+
+用户发送 "韭菜/暴富 添加/删除 基金/代码" 可以管理自己的基金列表
---
+++ plugin/chives/knex.js
@@ -0,0 +1,11 @@
+const path = require('path')
+const filename = path.join(__dirname, 'db.sqlite')
+const knex = require('knex')({
+  client: 'sqlite3',
+  connection: {
+    filename,
+  },
+  useNullAsDefault: true,
+})
+
+module.exports = { filename, knex }
