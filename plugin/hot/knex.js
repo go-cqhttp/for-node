e23975930ce941bcc2179fadbfae35ab111e370b
zhuweiyou <i@zhuweiyou.com>
2021-05-24 10:25:28 +0800

    rename

--- plugin/hot/knex.js
+++ plugin/hot/knex.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const filename = path.join(__dirname, 'word-cloud.sqlite')
+const filename = path.join(__dirname, 'db.sqlite')
 const knex = require('knex')({
   client: 'sqlite3',
   connection: {
