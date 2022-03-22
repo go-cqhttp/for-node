ead8a28618195e66468ab46647688283f1afd1f5
zhuweiyou <i@zhuweiyou.com>
2022-03-22 15:22:15 +0800

    修复问题

--- plugin/dapan/service.js
+++ plugin/dapan/service.js
@@ -16,9 +16,19 @@ async function getData() {
       width: 1000,
       height: 900,
     })
+    await page.setRequestInterception(true)
+    page.on('request', request => {
+      // http://sdc.jrj.com.cn/dcs0kbnws6cwv3t3c80mwlr23_8j4c/dcs.gif?dcsredirect=1&dcsdat=1647933490141&dcssip=summary.jrj.com.cn&dcsuri=/dpyt/&WT.tz=8&WT.bh=15&WT.ul=zh-CN&WT.cd=30&WT.sr=1536x960&WT.jo=No&WT.ti=%u5927%u76D8%u4E91%u56FE-%u884C%u60C5%u4E2D%u5FC3-%u91D1%u878D%u754C&WT.fi=No&WT.co_f=287052a4391d7fc08b51647933490142&WT.vt_f=1&WT.vt_f_a=1&WT.vt_f_s=1&WT.vt_f_d=1&WT.vt_sid=287052a4391d7fc08b51647933490142.1647933490142
+      if (request.url().includes('http://sdc.jrj.com.cn/')) {
+        request.abort()
+      } else {
+        request.continue()
+      }
+    })
     await page.goto(`http://summary.jrj.com.cn/dpyt/`)
     await page.addStyleTag({
       content: `
+        .header,
         body > :not(.main),
          .stock_inf,
          .jrj-where,
@@ -82,7 +92,7 @@ async function handler({ message }) {
 
 // 后台自动抓
 getData()
-setInterval(() => getData(), 1000 * 30)
+// setInterval(() => getData(), 1000 * 30)
 
 module.exports = {
   handler,
